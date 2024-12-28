13d14fae763d26b9dbc50e0c7371bb473951231c
suha jin <89185836+djm07073@users.noreply.github.com>
2024-12-29 01:45:41 +0900

    fix: use reccell in external worker

--- crates/revmc-worker/src/external.rs
+++ crates/revmc-worker/src/external.rs
@@ -1,4 +1,4 @@
-use std::{ fmt::Debug, num::NonZeroUsize, sync::{ Arc, RwLock } };
+use std::{ cell::RefCell, fmt::Debug, num::NonZeroUsize, rc::Rc, sync::{ Arc, RwLock } };
 
 use crate::{ error::ExtError, worker::{ aot_store_path, CompileWorker, SledDB } };
 use alloy_primitives::B256;
@@ -17,18 +17,24 @@ pub(crate) static SLED_DB: OnceCell<Arc<RwLock<SledDB<B256>>>> = OnceCell::new()
 #[derive(Debug)]
 pub struct EXTCompileWorker {
     compile_worker: Box<CompileWorker>,
-    pub cache: LruCache<B256, (EvmCompilerFn, libloading::Library)>,
+    pub cache: RefCell<LruCache<B256, (EvmCompilerFn, libloading::Library)>>,
 }
 
 impl EXTCompileWorker {
-    pub fn new(threshold: u64, max_concurrent_tasks: usize, cache_size_words: usize) -> Self {
+    pub fn new(
+        threshold: u64,
+        max_concurrent_tasks: usize,
+        cache_size_words: usize
+    ) -> Rc<RefCell<Self>> {
         let sled_db = SLED_DB.get_or_init(|| Arc::new(RwLock::new(SledDB::init())));
         let compiler = CompileWorker::new(threshold, Arc::clone(sled_db), max_concurrent_tasks);
 
-        Self {
-            compile_worker: Box::new(compiler),
-            cache: LruCache::new(NonZeroUsize::new(cache_size_words).unwrap()),
-        }
+        Rc::new(
+            RefCell::new(Self {
+                compile_worker: Box::new(compiler),
+                cache: RefCell::new(LruCache::new(NonZeroUsize::new(cache_size_words).unwrap())),
+            })
+        )
     }
 
     pub fn get_function(&mut self, code_hash: B256) -> Result<Option<EvmCompilerFn>, ExtError> {
@@ -36,7 +42,7 @@ impl EXTCompileWorker {
             return Ok(None);
         }
 
-        if let Some((f, _)) = self.cache.get(&code_hash) {
+        if let Some((f, _)) = self.cache.borrow_mut().get(&code_hash) {
             return Ok(Some(*f));
         }
 
@@ -54,9 +60,9 @@ impl EXTCompileWorker {
                 };
                 // The function holds a reference to the library, so dropping the library will cause an error.
                 // Therefore, the library must also be stored in the cache.
-                self.cache.put(code_hash, (f, lib));
+                self.cache.borrow_mut().put(code_hash, (f, lib));
 
-                if let Some((f, _)) = self.cache.get(&code_hash) {
+                if let Some((f, _)) = self.cache.borrow_mut().get(&code_hash) {
                     return Ok(Some(*f));
                 } else {
                     return Err(ExtError::LruCacheGetError);
--- crates/revmc-worker/src/worker/test.rs
+++ crates/revmc-worker/src/worker/test.rs
@@ -1,15 +1,20 @@
-use alloy_primitives::{address, Keccak256, Uint};
-use revm::{
-    db::{CacheDB, EmptyDB},
-    Evm,
-};
+use alloy_primitives::{ address, Keccak256, Uint };
+use revm::{ db::{ CacheDB, EmptyDB }, Evm };
 use revm_primitives::{
-    hex::FromHex, AccessList, AccessListItem, AccountInfo, BlockEnv, Bytecode, Bytes, TxEnv,
-    TxKind, B256,
+    hex::FromHex,
+    AccessList,
+    AccessListItem,
+    AccountInfo,
+    BlockEnv,
+    Bytecode,
+    Bytes,
+    TxEnv,
+    TxKind,
+    B256,
 };
 use std::thread;
 
-use crate::{register_handler, EXTCompileWorker};
+use crate::{ register_handler, EXTCompileWorker };
 
 fn setup_test_cache(ext_worker: &mut EXTCompileWorker, bytecode: &Bytecode) {
     let code_hash = bytecode.hash_slow();
@@ -41,27 +46,28 @@ fn fib_call_data() -> Bytes {
 
 #[test]
 fn test_compiler_cache_retrieval() {
-    let mut ext_worker = EXTCompileWorker::new(1, 3, 128);
+    let ext_worker = EXTCompileWorker::new(1, 3, 128);
     let bytecode = Bytecode::new_raw(Bytes::from_static(&[1, 2, 3]));
 
-    setup_test_cache(&mut ext_worker, &bytecode);
+    setup_test_cache(&mut ext_worker.borrow_mut(), &bytecode);
 }
 
 #[test]
 fn test_compiler_cache_load_access_list() {
-    let mut ext_worker = EXTCompileWorker::new(1, 3, 128);
+    let ext_worker = EXTCompileWorker::new(1, 3, 128);
 
     // Create
     let caller_address = address!("e100713fc15400d1e94096a545879e7c647001e0");
-    let fib_bin = "0x6080604052348015600e575f80fd5b5061020d8061001c5f395ff3fe608060405234801561000f575f80fd5b5060043610610029575f3560e01c806361047ff41461002d575b5f80fd5b610047600480360381019061004291906100f1565b61005d565b604051610054919061012b565b60405180910390f35b5f80820361006d575f90506100b5565b6001820361007e57600190506100b5565b61009360028361008e9190610171565b61005d565b6100a86001846100a39190610171565b61005d565b6100b291906101a4565b90505b919050565b5f80fd5b5f819050919050565b6100d0816100be565b81146100da575f80fd5b50565b5f813590506100eb816100c7565b92915050565b5f60208284031215610106576101056100ba565b5b5f610113848285016100dd565b91505092915050565b610125816100be565b82525050565b5f60208201905061013e5f83018461011c565b92915050565b7f4e487b71000000000000000000000000000000000000000000000000000000005f52601160045260245ffd5b5f61017b826100be565b9150610186836100be565b925082820390508181111561019e5761019d610144565b5b92915050565b5f6101ae826100be565b91506101b9836100be565b92508282019050808211156101d1576101d0610144565b5b9291505056fea26469706673582212201e1e9b4f1c72cf916978622f2e9e7e62e962bf691679559ba242ea8e9bf74c6164736f6c63430008190033";
+    let fib_bin =
+        "0x6080604052348015600e575f80fd5b5061020d8061001c5f395ff3fe608060405234801561000f575f80fd5b5060043610610029575f3560e01c806361047ff41461002d575b5f80fd5b610047600480360381019061004291906100f1565b61005d565b604051610054919061012b565b60405180910390f35b5f80820361006d575f90506100b5565b6001820361007e57600190506100b5565b61009360028361008e9190610171565b61005d565b6100a86001846100a39190610171565b61005d565b6100b291906101a4565b90505b919050565b5f80fd5b5f819050919050565b6100d0816100be565b81146100da575f80fd5b50565b5f813590506100eb816100c7565b92915050565b5f60208284031215610106576101056100ba565b5b5f610113848285016100dd565b91505092915050565b610125816100be565b82525050565b5f60208201905061013e5f83018461011c565b92915050565b7f4e487b71000000000000000000000000000000000000000000000000000000005f52601160045260245ffd5b5f61017b826100be565b9150610186836100be565b925082820390508181111561019e5761019d610144565b5b92915050565b5f6101ae826100be565b91506101b9836100be565b92508282019050808211156101d1576101d0610144565b5b9291505056fea26469706673582212201e1e9b4f1c72cf916978622f2e9e7e62e962bf691679559ba242ea8e9bf74c6164736f6c63430008190033";
     let db = CacheDB::new(EmptyDB::new());
 
     let fib_bytecode = Bytecode::new_raw(fib_bin.into());
     let fib_hash = fib_bytecode.hash_slow();
-    setup_test_cache(&mut ext_worker, &fib_bytecode);
+    setup_test_cache(&mut ext_worker.borrow_mut(), &fib_bytecode);
 
     let mut evm = Evm::builder()
-        .with_external_context(EXTCompileWorker::new(1, 3, 128))
+        .with_external_context(ext_worker.clone())
         .with_db(db)
         .append_handler_register(register_handler)
         .build();
@@ -79,30 +85,29 @@ fn test_compiler_cache_load_access_list() {
 
     // Manually insert account info for deployed contract
     // Not necessary in practice
-    evm.db_mut().insert_account_info(
-        deployed_address,
-        AccountInfo {
-            code_hash: fib_bytecode.hash_slow(),
-            code: Some(fib_bytecode),
-            ..Default::default()
-        },
-    );
+    evm.db_mut().insert_account_info(deployed_address, AccountInfo {
+        code_hash: fib_bytecode.hash_slow(),
+        code: Some(fib_bytecode),
+        ..Default::default()
+    });
 
     // Call
-    let access_list = AccessList(vec![AccessListItem {
-        address: deployed_address,
-        storage_keys: vec![B256::ZERO],
-    }]);
+    let access_list = AccessList(
+        vec![AccessListItem {
+            address: deployed_address,
+            storage_keys: vec![B256::ZERO],
+        }]
+    );
 
     // Cache upfront
-    if let Err(err) = ext_worker.preload_cache(vec![fib_hash]) {
+    if let Err(err) = ext_worker.borrow_mut().preload_cache(vec![fib_hash]) {
         println!("While cache_load_access_list: {:#?}", err);
-    };
+    }
     thread::sleep(std::time::Duration::from_secs(2));
 
     evm.context.evm.inner.env.tx.transact_to = TxKind::Call(deployed_address);
     evm.context.evm.inner.env.tx.data = fib_call_data();
-    evm.context.evm.inner.env.tx.access_list = access_list.0.clone();
+    evm.context.evm.inner.env.tx.access_list = access_list.to_vec();
 
     if let Err(err) = evm.transact() {
         println!("While evm transact: {:#?}", err)
