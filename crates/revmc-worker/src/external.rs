c8c8dfb54ce861503058a79f5001f206569362c7
suha jin <89185836+djm07073@users.noreply.github.com>
2024-12-29 00:53:06 +0900

    fix: clippy & add some comments

--- crates/revmc-worker/src/external.rs
+++ crates/revmc-worker/src/external.rs
@@ -1,13 +1,6 @@
-use std::{
-    fmt::Debug,
-    num::NonZeroUsize,
-    sync::{Arc, RwLock},
-};
+use std::{ fmt::Debug, num::NonZeroUsize, sync::{ Arc, RwLock } };
 
-use crate::{
-    error::ExtError,
-    worker::{aot_store_path, CompileWorker, SledDB},
-};
+use crate::{ error::ExtError, worker::{ aot_store_path, CompileWorker, SledDB } };
 use alloy_primitives::B256;
 use lru::LruCache;
 use once_cell::sync::OnceCell;
@@ -16,11 +9,11 @@ use revmc::EvmCompilerFn;
 
 pub(crate) static SLED_DB: OnceCell<Arc<RwLock<SledDB<B256>>>> = OnceCell::new();
 
-/// Compiler Worker as external context.
+/// Compiler Worker as an external context.
 ///
-/// ExternalFn fetching is optimized by using LRU Cache
-/// Heavily reduces disk I/O
-
+/// External function fetching is optimized by using an LRU Cache.
+/// In many cases, a contract that is called will likely be called again,
+/// so the cache helps reduce disk I/O cost.
 #[derive(Debug)]
 pub struct EXTCompileWorker {
     compile_worker: Box<CompileWorker>,
@@ -47,17 +40,20 @@ impl EXTCompileWorker {
             return Ok(Some(*f));
         }
 
-        let so_file = aot_store_path().join(&code_hash.to_string()).join("a.so");
+        let so_file = aot_store_path().join(code_hash.to_string()).join("a.so");
         let exist: bool = so_file.try_exists().unwrap_or(false);
         if exist {
             {
-                let lib = (unsafe { libloading::Library::new(&so_file) })
-                    .map_err(|err| ExtError::LibLoadingError { err: err.to_string() })?;
+                let lib = (unsafe { libloading::Library::new(&so_file) }).map_err(
+                    |err| ExtError::LibLoadingError { err: err.to_string() }
+                )?;
                 let f: EvmCompilerFn = unsafe {
-                    *lib.get(code_hash.to_string().as_ref())
+                    *lib
+                        .get(code_hash.to_string().as_ref())
                         .map_err(|err| ExtError::GetSymbolError { err: err.to_string() })?
                 };
-
+                // The function holds a reference to the library, so dropping the library will cause an error.
+                // Therefore, the library must also be stored in the cache.
                 self.cache.put(code_hash, (f, lib));
 
                 if let Some((f, _)) = self.cache.get(&code_hash) {
@@ -65,7 +61,7 @@ impl EXTCompileWorker {
                 } else {
                     return Err(ExtError::LruCacheGetError);
                 };
-            };
+            }
         }
 
         Ok(None)
