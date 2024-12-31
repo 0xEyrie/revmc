975bf94d08cae38db1b41a6b0bb12bee122218f9
Ji Hwan An <angeljihwan@gmail.com>
2024-12-31 11:02:19 +0900

    examples: add working worker example

--- examples/worker/src/main.rs
+++ examples/worker/src/main.rs
@@ -1,8 +1,5 @@
 //! Simple compiler worker example.
 
-use std::thread;
-
-use common::{FIBONACCI_CODE, FIBONACCI_HASH};
 use revm::{
     db::{CacheDB, EmptyDB},
     primitives::{
@@ -10,9 +7,19 @@ use revm::{
     },
 };
 use revmc_worker::{register_handler, EXTCompileWorker};
+use std::thread;
+
+use common::FIBONACCI_CODE;
 
 mod common;
 
+/// First call executes the transaction and compiles into embedded db
+/// embedded db: ~/.aotstore/db, ~/.aotstore/output
+/// It is crucial to reset the embedded db for reproducing the same steps
+/// Otherwise, both calls will utilize cached ExternalFn
+///
+/// Second call loads the ExternalFn from embedded db to cache
+/// and executes transaction with it
 fn main() {
     let ext_worker = EXTCompileWorker::new(1, 3, 128);
 
@@ -24,10 +31,14 @@ fn main() {
         .build();
 
     let fibonacci_address = address!("0000000000000000000000000000000000001234");
+
+    let fib_bytecode = Bytecode::new_raw(FIBONACCI_CODE.into());
+    let fib_hash = fib_bytecode.hash_slow();
+
     evm.db_mut().insert_account_info(
         fibonacci_address,
         AccountInfo {
-            code_hash: FIBONACCI_HASH.into(),
+            code_hash: fib_hash.into(),
             code: Some(Bytecode::new_raw(FIBONACCI_CODE.into())),
             ..Default::default()
         },
@@ -38,11 +49,17 @@ fn main() {
         storage_keys: vec![B256::ZERO],
     }]);
 
-    ext_worker.preload_cache(vec![B256::from(FIBONACCI_HASH)]).unwrap();
+    // First call - compiles ExternalFn
+    evm.context.evm.env.tx.transact_to = TransactTo::Call(fibonacci_address);
+    evm.context.evm.env.tx.data = U256::from(9).to_be_bytes_vec().into();
+    evm.context.evm.inner.env.tx.access_list = access_list.to_vec();
     thread::sleep(std::time::Duration::from_secs(2));
 
+    ext_worker.preload_cache(vec![B256::from(fib_hash)]).unwrap();
+
+    // Second call - uses cached ExternalFn
     evm.context.evm.env.tx.transact_to = TransactTo::Call(fibonacci_address);
-    evm.context.evm.env.tx.data = U256::from(10).to_be_bytes_vec().into();
+    evm.context.evm.env.tx.data = U256::from(9).to_be_bytes_vec().into();
     evm.context.evm.inner.env.tx.access_list = access_list.to_vec();
 
     let result = evm.transact().unwrap();
